12b99f2fc247c1ae2a9ad01ff5cce117eb65f6fb
glody007 <glodymbutwile@gmail.com>
2021-07-27 11:46:32 +0400

    bottom navigation

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Home</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+function MarketScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Market</Text>
+    </View>
+  );
+}
+
+function TransactionsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Transactions</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Market" component={MarketScreen} />
+        <Tab.Screen name="Transactions" component={TransactionsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
