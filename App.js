9216cdd8ef76fefdd37032cdf5ebc28397dd2242
glody007 <glodymbutwile@gmail.com>
2021-07-27 12:28:34 +0400

    Drawer navigation

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 function HomeScreen() {
@@ -31,14 +32,51 @@ function TransactionsScreen() {
 
 const Tab = createBottomTabNavigator();
 
+function MainScreen() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Market" component={MarketScreen} />
+      <Tab.Screen name="Transactions" component={TransactionsScreen} />
+    </Tab.Navigator>
+  );
+}
+
+function ProfileScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Profile</Text>
+    </View>
+  );
+}
+
+function PayementScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Methode de paiement</Text>
+    </View>
+  );
+}
+
+function ParametresScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Parametres</Text>
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Market" component={MarketScreen} />
-        <Tab.Screen name="Transactions" component={TransactionsScreen} />
-      </Tab.Navigator>
+      <Drawer.Navigator initialRouteName="Main">
+        <Drawer.Screen name="Profile" component={ProfileScreen} />
+        <Drawer.Screen name="Main" component={MainScreen} />
+        <Drawer.Screen name="Methode de paiement" component={PayementScreen} />
+        <Drawer.Screen name="Parametres" component={ParametresScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
